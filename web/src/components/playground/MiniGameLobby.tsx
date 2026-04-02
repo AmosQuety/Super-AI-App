5df304c429af269a0c574c5f9e27f195b2aab3d2
AmosQuety <amosnabasa4@gmail.com>
2026-04-02 21:29:39 +0300

    Fixed ts error

--- web/src/components/playground/MiniGameLobby.tsx
+++ web/src/components/playground/MiniGameLobby.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import SnakeGame from './SnakeGame';
 import PongGame from './PongGame';
 import { Gamepad2 } from 'lucide-react';
